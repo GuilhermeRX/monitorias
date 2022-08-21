401aa8740fffbf2d5ecf56dd3d89240251ee12e0
Luiz Guilherme <guilhermerx18@gmail.com>
2022-08-21 15:48:54 -0300

    book controller ok

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,22 +1,34 @@
+const bookService = require("../services/bookService");
+
 const bookController = {
-  create: (req, res) => {
-    res.status(201).json()
+  create: async (req, res) => {
+    const book = req.body
+    const insertId = await bookService.create(book);
+    res.status(201).json({ id: insertId, ...book })
   },
 
-  delete: (req, res) => {
-    res.status(204).json()
+  delete: async (req, res) => {
+    const id = req.params.id
+    await bookService.delete(id);
+    res.status(204).json({ message: 'Livro Deletado!' })
   },
 
-  update: (req, res) => {
-    res.status(200).json()
+  update: async (req, res) => {
+    const book = req.body;
+    const id = req.params.id;
+    await bookService.update(book, id);
+    res.status(200).json({ id, ...book })
   },
 
-  findById: (req, res) => {
-    res.status(200).json()
+  findById: async (req, res) => {
+    const id = req.params.id;
+    const book = await bookService.findById(id);
+    res.status(200).json(book);
   },
 
-  findAll: (req, res) => {
-    res.status(200).json()
+  findAll: async (req, res) => {
+    const books = await bookService.findAll();
+    res.status(200).json(books)
   },
 };
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,32 @@
 const userService = require("../services/userService");
 
 const userController = {
-  create: (req, res) => {
+  create: async (req, res) => {
     const user = req.body;
     const insertId = await userService.create(user);
     res.status(201).json({ id: insertId, ...user });
   },
 
-  delete: (req, res) => {
+  delete: async (req, res) => {
     const id = req.params.id;
     await userService.delete(id);
     res.status(204).json({ message: 'Usuário deletado!' });
   },
 
-  update: (req, res) => {
+  update: async (req, res) => {
     const user = req.body;
     const id = req.params.id;
     await userService.update(user, id);
     res.status(200).json({ id, ...user });
   },
 
-  findById: (req, res) => {
+  findById: async (req, res) => {
     const id = req.params.id;
     const user = await userService.findById(id);
     res.status(200).json(user);
   },
 
-  findAll: (req, res) => {
+  findAll: async (req, res) => {
     const users = await userService.findAll(users);
     res.status(200).json(users);
   },
