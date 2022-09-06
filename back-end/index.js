4338df744d3c1c3b111018fdbac276b72eb259bc
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-06 11:15:42 -0300

    feat cors

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,13 @@
 require('dotenv/config')
 const express = require('express')
+const cors = require('cors');
+
 const { User } = require('./models/')
 const app = express()
 const port = process.env.PORT
+app.use(cors());
 app.use(express.json())
+
 
 app.get('/', async (req, res) => {
   const users = await User.findAll()
--- front-heroku/src/App.js
+++ front-heroku/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import logo from './logo.svg';
 
 function App() {
+  const users = async () => {
+    const users = await fetch('https://guilhermerx-back.herokuapp.com/');
+    const data = await users.json();
+    return data;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Heroku.
+          {users}
         </p>
         <a
           className="App-link"
